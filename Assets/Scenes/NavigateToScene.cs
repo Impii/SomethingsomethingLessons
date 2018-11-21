0bd074b70cf89da87884afdc291ef9946084cc2f
Imme <imme_50@hotmail.com>
2018-11-21 16:12:42 +0100

    Beepboop
    Boop

---
+++ Assets/Scenes/NavigateToScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigateToScene : MonoBehaviour
+{
+
+    [SerializeField]
+
+    string SceneToNavigateTo;
+
+    public void Navigate()
+    {
+        SceneManager.LoadScene(SceneToNavigateTo);
+    }
+}
+
